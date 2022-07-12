4ae8c95a9766f290cd34ba75e3b2fdd788c8401d
Hassan-Hajir <hashajir@.com>
2022-07-12 00:07:19 -0700

    used switch to fix errors

--- _printf.c
+++ _printf.c
@@ -1,45 +1,50 @@
 #include "main.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
  *_printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
  * Return: the length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+	int count = 0, i;
 
-	va_list args;
-	int i = 0, j, len = 0;
+	va_list data;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+	va_start(data, format);
 
-	while (format[i] != '\0')
+	for (i = 0; format[i] != '\0'; )
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[i] != '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			count += _putchar(format[i]);
+			i++;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else if (format[i] == '%' && format[i + 1] != ' ')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					count += _putchar(va_arg(data, int));
+					break;
+				case 's':
+					count += print_string(va_arg(data, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 'd':
+					count += print_decimal(va_arg(data, int));
+					break;
+				case 'i':
+					count += print_decimal(va_arg(data, int));
+					break;
+				default:
+					break;
+			}
+			i += 2;
+		}
 	}
-	va_end(args);
-	return (len);
+	return (count);
 }
